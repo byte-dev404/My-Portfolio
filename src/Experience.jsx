d09de97ff2d41e6b0a1ff32b20342a1cff9f94ba
Vishwas Batra / Dev-404 <zendiagogamingbackup@gmail.com>
2025-11-22 08:10:08 +0530

    Responsive-Navbar & routing added

---
+++ src/Experience.jsx
@@ -0,0 +1,16 @@
+import NavBar from './Nav-bar.jsx'
+
+function Experience() {
+    
+    return(
+        <>
+            <NavBar/>
+            <h1>Experience page</h1>
+            <h1>Experience page</h1>
+            <h1>Experience page</h1>
+            <h1>Experience page</h1>
+        </>
+    );
+}
+
+export default Experience
---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import NavBar from './Nav-bar.jsx'
+
+function Home() {
+
+    return(
+        <>
+            <NavBar/>
+            <h1>Home page</h1>
+            <h1>Home page</h1>
+            <h1>Home page</h1>
+            <h1>Home page</h1>
+        </>
+    );
+}
+
+export default Home
